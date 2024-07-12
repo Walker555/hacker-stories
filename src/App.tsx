621c43accb4e99fbf1dbceb23ea984abad1acd88
Jozef Hribik <jozef.hribik@collumino.com>
2024-07-12 11:16:25 +0200

    React JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-// const title = 'React';
+const title = 'React';
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
